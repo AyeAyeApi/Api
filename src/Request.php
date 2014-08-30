e9df1a4384b96f16caf4a3f42896b6743adec93e
Daniel Mason <daniel@danielmason.com>
2014-08-30 21:17:00 +0100

    getRequestChain was returning nothing

--- src/Request.php
+++ src/Request.php
@@ -235,7 +235,7 @@ class Request implements \JsonSerializable
      */
     public function getRequestChain() {
         if(is_null($this->requestChain)) {
-            $this->getRequestChainFromUri($this->requestedUri);
+            $this->requestChain = $this->getRequestChainFromUri($this->requestedUri);
         }
         return $this->requestChain;
     }
