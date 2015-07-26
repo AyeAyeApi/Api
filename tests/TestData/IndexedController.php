518a44912d82ea10d47604d416e6253267a055f7
Daniel Mason <daniel@danielmason.com>
2015-07-26 10:31:37 +0000

    Missed the TestData for commit b2e36d5c8fafec1b4554134e7e4d631a1fa57290

--- tests/TestData/IndexedController.php
+++ tests/TestData/IndexedController.php
@@ -8,28 +8,25 @@ namespace AyeAye\Api\Tests\TestData;
 
 use AyeAye\Api\Controller;
 
-class DocumentedController extends Controller {
+class IndexedController extends Controller {
+
 
     /**
-     * Test Summary
-     * Test Description.
-     * @param $incomplete
-     * @param int    $int    Test integer
-     * @param string $string Test string
+     * The GET index
      * @return string
      */
-    public function getDocumentedEndpoint($incomplete, $int, $string)
+    public function getIndexEndpoint()
     {
-        return "information";
+        return 'Got Index';
     }
 
     /**
-     * Recursive self reference controller
-     * @return $this
+     * The PUT index
+     * @return string
      */
-    public function selfReferenceController()
+    public function putIndexEndpoint()
     {
-        return $this;
+        return 'Put Index';
     }
 
 }
