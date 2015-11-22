97adceed9411f64707be1b065e43bd52ce0b357c
Daniel Mason <daniel@danielmason.com>
2015-11-22 11:45:01 +0000

    This test is also ok, but renamed method in Request to be more accurate. Xml problem fixed by Formatters module

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -223,63 +223,61 @@ class RequestTest extends TestCase
 
     /**
      * @test
-     * @covers ::stringToObject
+     * @covers ::stringToArray
      * @uses \AyeAye\Api\Request
      */
-    public function testStringToClass()
+    public function testStringToArray()
     {
         $request = new Request();
-        $stringToObject = $this->getObjectMethod($request, 'stringToObject');
+        $stringToArray = $this->getObjectMethod($request, 'stringToArray');
 
 
         $this->assertArrayNotHasKey(
             'text',
-            $stringToObject('')
+            $stringToArray('')
         );
 
         $this->assertArrayHasKey(
             'text',
-            $stringToObject('test')
+            $stringToArray('test')
         );
         $this->assertSame(
             'test',
-            $stringToObject('test')['text']
+            $stringToArray('test')['text']
         );
 
         $json = '{"key":"value"}';
 
         $this->assertArrayNotHasKey(
             'text',
-            $stringToObject($json)
+            $stringToArray($json)
         );
         $this->assertArrayHasKey(
             'key',
-            $stringToObject($json)
+            $stringToArray($json)
         );
         $this->assertSame(
             'value',
-            $stringToObject($json)['key']
+            $stringToArray($json)['key']
         );
 
         $xml = '<container><key>anotherValue</key></container>';
 
         $this->assertArrayNotHasKey(
             'text',
-            $stringToObject($xml)
+            $stringToArray($xml)
         );
         $this->assertArrayHasKey(
             'key',
-            $stringToObject($xml)
+            $stringToArray($xml)
         );
 
-        // ToDo: How does this even???
-//        /** @var \SimpleXMLElement $object */
-//        $object = $stringToObject($xml);
-//        print_r($object->children()->text()); die;
-//        $this->assertSame(
-//            'anotherValue',
-//            $stringToObject($xml)->key[0]
-//        );
+//         ToDo: How does this even???
+        /** @var \SimpleXMLElement $object */
+        $this->assertSame(
+            'anotherValue',
+            $stringToArray($xml)['key']
+        );
     }
 
     /**
